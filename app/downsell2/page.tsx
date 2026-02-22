706f12ac9b94f0f8eac557c7c9a5ce98c2258526
Pedro Castro <pedro.b.041196@gmail.com>
2026-02-22 09:41:51 -0300

    criar paginas de upsell, downsell e thanks

---
+++ app/downsell2/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import Script from "next/script"
+
+export default function Downsell2Page() {
+    return (
+        <>
+            {/* --- sales funnel container (head) --- */}
+            <Script
+                src="https://checkout.hotmart.com/lib/hotmart-checkout-elements.js"
+                strategy="afterInteractive"
+            />
+            <Script id="hotmart-sales-funnel-init-ds2" strategy="afterInteractive">
+                {`checkoutElements.init('salesFunnel').mount('#hotmart-sales-funnel')`}
+            </Script>
+
+            {/* --- body --- */}
+            {/* HOTMART - Sales Funnel Widget */}
+            <div id="hotmart-sales-funnel"></div>
+        </>
+    )
+}
