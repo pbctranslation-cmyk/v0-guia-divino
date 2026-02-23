6afa07f54c702cc19fdabe1b5d68301a8f5ca137
Pedro Castro <pedro.b.041196@gmail.com>
2026-02-23 18:26:15 -0300

    incluir utmify e pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata, Viewport } from 'next'
 import { Playfair_Display, Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
+import { FacebookPixelEvents } from '@/components/fb-pixel-events'
+import { metadata as siteMetadata, viewport as siteViewport } from './metadata'
 import './globals.css'
 
 const _playfair = Playfair_Display({
@@ -12,31 +15,8 @@ const _inter = Inter({
   variable: '--font-sans',
 })
 
-export const metadata: Metadata = {
-  title: 'Quiz de Religiao',
-  description: 'Teste seus conhecimentos sobre religiao com 25 perguntas',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
-}
-
-export const viewport: Viewport = {
-  themeColor: '#141414',
-}
+export const metadata: Metadata = siteMetadata
+export const viewport: Viewport = siteViewport
 
 export default function RootLayout({
   children,
@@ -45,7 +25,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* UTMify Script */}
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-subids
+          strategy="afterInteractive"
+        />
+
+        {/* Facebook Pixel Base */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '24729655026695075');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=24729655026695075&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={`${_playfair.variable} ${_inter.variable} font-sans antialiased`}>
+        <FacebookPixelEvents />
         {children}
         <Analytics />
       </body>
